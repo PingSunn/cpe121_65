99231e1fa5a7599ae4ed925117a857ead2fb1d00
Pongsakorn Weraburudpongsatorn <ping_pongsakorn@hotmail.com>
2023-04-28 01:32:32 +0700

    Update ex8_2.java

--- ex8/ex8_2.java
+++ ex8/ex8_2.java
@@ -7,48 +7,49 @@ public class ex8_2 {
         System.out.println(soapFlower); 
         System.out.println(carrie);
     }
-    public static class Flower {
+}
+class Flower {
 
-        private String useInstruction;
+    private String useInstruction;
 
-        public Flower(String instruction) {
-            this.useInstruction = instruction;
-        } 
-        public String getInstruction(){
-            return this.useInstruction;
-        }
-    }
-
-    public static class WallFlower extends Flower{
-        public WallFlower() {
-           super("Talk to me for some interesting information.");
-        }
-        public String toString(){
-            return "Wallflower: " + super.getInstruction();
-        }
-    }
-
-    public static class CauliFlower extends Flower{
-        private double price;
-        public CauliFlower(String instruction, double perKgPrice) {
-            super(instruction);
-            this.price = perKgPrice;
-        }
-        public String toString(){
-            return "Cauliflower : Price " + this.price + " baht/kg" + super.getInstruction();
-        }
-    }
-
-    public static class CarvedFlower extends Flower{
-        private double price;
-        private String material;
-        public CarvedFlower(String instruction, String material, double perUnitPrice){
-            super(instruction);
-            this.material = material;
-            this.price = perUnitPrice;
-        }
-        public String toString() {
-            return "Carved flower: Price" + price + "$US. Material = "+ material + super.getInstruction();
-        }
+    public Flower(String instruction) {
+        this.useInstruction = instruction;
+    } 
+    public String getInstruction(){
+        return this.useInstruction;
     }
 }
+
+class WallFlower extends Flower{
+    public WallFlower() {
+        super("Talk to me for some interesting information.");
+    }
+    public String toString(){
+        return "Wallflower: " + super.getInstruction();
+    }
+}
+
+class CauliFlower extends Flower{
+    private double price;
+    public CauliFlower(String instruction, double perKgPrice) {
+        super(instruction);
+        this.price = perKgPrice;
+    }
+    public String toString(){
+        return "Cauliflower : Price " + this.price + " baht/kg" + super.getInstruction();
+    }
+}
+
+class CarvedFlower extends Flower{
+    private double price;
+    private String material;
+    public CarvedFlower(String instruction, String material, double perUnitPrice){
+        super(instruction);
+        this.material = material;
+        this.price = perUnitPrice;
+    }
+    public String toString() {
+        return "Carved flower: Price" + price + "$US. Material = "+ material + super.getInstruction();
+    }
+}
+
